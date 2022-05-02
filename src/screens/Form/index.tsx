5ee329b16b66b6c50e95c90a3d64aaf24397576f
thomascostta <thomas.jeffcosta@gmail.com>
2022-05-02 18:50:07 -0300

    settings

--- src/screens/Form/index.tsx
+++ src/screens/Form/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
 import Toast from 'react-native-toast-message';
 import uuid from 'react-native-uuid';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { styles } from './styles';
 
@@ -10,6 +11,27 @@ import { Button } from '../../components/Button';
 import { HeaderForm } from '../../components/HeaderForm';
 
 export function Form() {
+  const [name, setName] = useState("");
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleNew() {
+    const id = uuid.v4();
+
+    const newData = {
+      id,
+      name,
+      user,
+      password
+    }
+
+    await AsyncStorage.setItem("@savepass:passwords", JSON.stringify(newData));
+
+    Toast.show({
+      type: 'success',
+      text1: 'Cadastrado com sucesso'
+    })
+  }
 
 
   return (
@@ -25,20 +47,24 @@ export function Form() {
           <View style={styles.form}>
             <Input
               label="Nome do serviço"
+              onChangeText={setName}
             />
             <Input
               label="E-mail ou usuário"
               autoCapitalize="none"
+              onChangeText={setUser}
             />
             <Input
               label="Senha"
               secureTextEntry
+              onChangeText={setPassword}
             />
           </View>
 
           <View style={styles.footer}>
             <Button
               title="Salvar"
+              onPress={handleNew}
             />
           </View>
         </ScrollView>
